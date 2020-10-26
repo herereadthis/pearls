5885652b92db827b5719f71b1f2832418f578067
herereadthis <herereadthis@gmail.com>
2020-10-26 00:44:07 -0400

    logging

--- scripts/run-dist.js
+++ scripts/run-dist.js
@@ -2,59 +2,11 @@ const shell = require('shelljs');
 const minify = require('@node-minify/core');
 const cleanCSS = require('@node-minify/clean-css');
 const htmlMinifier = require('@node-minify/html-minifier');
+const replace = require('replace-in-file');
+const logger = require('../src/utils/logger');
 
 
 shell.config.fatal = true;
-
-try {
-    shell.exec('rm -rf dist');
-    shell.exec('mkdir dist');
-    shell.exec('./node_modules/less/bin/lessc ./src/less/styles.less ./dist/styles.css');
-    minify({
-        compressor: cleanCSS,
-        input: './dist/styles.css',
-        output: './dist/styles.min.css',
-        callback: function(err, min) {}
-    });
-    minify({
-        compressor: htmlMinifier,
-        input: './index.html',
-        output: './dist/index.html',
-        callback: function(err, min) {}
-    });
-} catch (err) {
-    console.error('Something bad happened');
-    console.error(err);
-    process.exit(1);
-}
-
-
-// const minify = require('@node-minify/core');
-// const cleanCSS = require('@node-minify/clean-css');
-// const htmlMinifier = require('@node-minify/html-minifier');
-
-/*
-
- shell.config.fatal = true;
-
- try {
- // shell.exec('cd ./src');
- shell.exec('node /node_modules/http-server/bin/http-server ./ --port 8090');
- } catch (err) {
- console.error('Something bad happened');
- console.error(err);
- process.exit(1);
- }
-
-
- if (argv.ships > 3 && argv.distance < 53.5) {
- console.log('Plunder more riffiwobbles!');
- } else {
- console.log('Retreat from the xupptumblers!');
- }
-
- */
-
 
 
 const yargs = require('yargs/yargs');
@@ -68,7 +20,7 @@ try {
     // shell.exec('cd ./src');
     // shell.exec('node ./node_modules/less/bin/lessc ./src/less/styles.less ./styles.css');
     // shell.exec('node ./node_modules/http-server/bin/http-server ./ --port ' + argv.port);
-
+    logger.info('creating dist folder');
     shell.exec('rm -rf dist');
     shell.exec('mkdir dist');
     shell.exec('./node_modules/less/bin/lessc ./src/less/styles.less ./dist/styles.css');
@@ -78,20 +30,29 @@ try {
         output: './dist/styles.min.css',
         callback: function(err, min) {}
     });
+    shell.exec('rm ./dist/styles.css');
     minify({
         compressor: htmlMinifier,
         input: './index.html',
         output: './dist/index.html',
         callback: function(err, min) {}
     });
+    replace.sync({
+        files: './dist/index.html',
+        from: /temp\/styles.css/,
+        to: 'styles.min.css'
+    });
+
+
     if (argv.port !== undefined) {
+        logger.info('starting server at http://localhost:' + argv.port);
         shell.exec('node ./node_modules/http-server/bin/http-server ./dist --port ' + argv.port);
     }
 
 
 } catch (err) {
-    console.error('Something bad happened');
-    console.error(err);
+    logger.error('Something bad happened!');
+    logger.error(err);
     process.exit(1);
 }
 
--- scripts/run-local.js
+++ scripts/run-local.js
@@ -1,34 +1,7 @@
 const shell = require('shelljs');
-// const minify = require('@node-minify/core');
-// const cleanCSS = require('@node-minify/clean-css');
-// const htmlMinifier = require('@node-minify/html-minifier');
-
-/*
-
-shell.config.fatal = true;
-
-try {
-    // shell.exec('cd ./src');
-    shell.exec('node /node_modules/http-server/bin/http-server ./ --port 8090');
-} catch (err) {
-    console.error('Something bad happened');
-    console.error(err);
-    process.exit(1);
-}
-
-
-if (argv.ships > 3 && argv.distance < 53.5) {
-    console.log('Plunder more riffiwobbles!');
-} else {
-    console.log('Retreat from the xupptumblers!');
-}
-
-    */
-
-
-
-const yargs = require('yargs/yargs');
 const helpers = require('yargs/helpers');
+const yargs = require('yargs/yargs');
+
 const hideBin = helpers.hideBin;
 const argv = yargs(hideBin(process.argv)).argv;
 
@@ -36,7 +9,7 @@ console.log(argv);
 
 try {
     // shell.exec('cd ./src');
-    shell.exec('node ./node_modules/less/bin/lessc ./src/less/styles.less ./styles.css');
+    shell.exec('node ./node_modules/less/bin/lessc ./src/less/styles.less ./temp/styles.css');
     shell.exec('node ./node_modules/http-server/bin/http-server ./ --port ' + argv.port);
 } catch (err) {
     console.error('Something bad happened');
